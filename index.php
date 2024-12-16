5fc5d98c8805a008113f28e765c3e621aa4233be
selvam <kamaleshwaranilaiyaraja@goodapptaxis.com>
2024-12-16 12:18:10 +0530

    changes

--- index.php
+++ index.php
@@ -264,9 +264,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   <!-- FOOTER -->
   <footer class="container">
     <p class="float-end"><a href="#">Back to top</a></p>
-    <p>&copy; 2017–2021 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+    <p>&copy; 2017–2021 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms and Condition</a></p>
   </footer>
 </main>
+
 
 
     <script src="assets/dist/js/bootstrap.bundle.min.js"></script>
